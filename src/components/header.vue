2f998d7469f8358bfede92a3cab17fa125f0da06
XtLee <lixt1098@163.com>
2017-12-28 16:29:01 +0800

    git install element-ui

---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="header">
+    <h1>RESUMER</h1>
+    <div class="buttons">
+      <el-button type="primary">主要按钮</el-button>
+      <el-button plain>朴素按钮</el-button>
+    </div>
+  </div>
+</template>
+
+
+
+<style lang="less">
+
+</style>
+
+
