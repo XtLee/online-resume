c486af778c7373cf90aeefbb992a050fc37343d3
XtLee <lixt1098@163.com>
2018-01-02 14:55:41 +0800

    重构工作经历

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -14,20 +14,7 @@
 				<profileEditor v-bind:profile="profile"/>
 			</li>
 			<li v-bind:class="{active: currentTab === 1}">
-				<h2>工作经历</h2>
-				<el-form>
-					<div class="workExperience" v-for="(work, index) in workExperience">
-						<el-form-item label="公司名称">
-							<el-input v-model="work.company"></el-input>
-						</el-form-item>
-						<el-form-item label="工作内容">
-							<el-input v-model="work.content"></el-input>
-						</el-form-item>
-						<el-button class="remove" type="primary" icon="el-icon-delete" v-on:click="removeWorkExperience(index)"></el-button>
-						<hr>
-					</div>
-					<el-button type="primary" v-on:click="addWorkExperience">添加</el-button>
-				</el-form>
+				<workExperience v-bind:workExperience="workExperience"/>
 			</li>
 			<li v-bind:class="{active: currentTab === 2}">
 				<h2>学习经历</h2>
@@ -89,8 +76,11 @@
 
 <script>
 import profileEditor from './profileEditor.vue'
+import workExperience from './workExperience.vue'
 export default {
-	components: { profileEditor },
+	components: { 
+		profileEditor, workExperience
+		},
   data() {
     return {
       currentTab: 0,
@@ -110,15 +100,7 @@ export default {
     };
   },
 	methods: {
-		addWorkExperience() {
-			this.workExperience.push({
-				company: '',
-				content: ''
-			})
-		},
-		removeWorkExperience(index) {
-			this.workExperience.splice(index, 1)
-		}
+		
 	}
 };
 </script>
